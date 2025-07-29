4bfbb43097c1cabf548f009a6f5327ca48bbfae3
KANTNOLI <zahargysev02@gmail.com>
2025-07-29 21:00:35 +0300

    1

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -7,6 +7,27 @@ function AboutMe() {
 
     return (
         <section className={style.AboutMe}>
+
+            <section className={style.Preview}>
+                <div className={style.PreviewFPart}>
+                    <p className={style.PreviewFPartTitle}>Ready to turn your broken heart into a confident, peaceful one?</p>
+                    <p className={style.PreviewFPartTitleDesc}>Grab your 30 minutes with me now and lets get started.</p> <div className={style.customLine}></div>
+
+                    <p className={style.PreviewFPartDesc}>Invest 16 hours over the next 4 months, and you’ll gain
+                    </p>
+                    <p className={style.PreviewFPartDesc}>350,000 future hours infinitely better spent with your family.
+                    </p>
+
+                    <button className={style.PreviewFPartBtn}>Schedule your free call</button>
+                </div>
+
+                <div className={style.PreviewSPart}>
+                    {/* IMG */}
+                    w 500px
+                </div>
+
+            </section>
+
         </section>
     )
 }
