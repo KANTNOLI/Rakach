d20279d89f99533ab1aedd766ef53be8d9b36259
KANTNOLI <zahargysev02@gmail.com>
2025-07-29 18:45:16 +0300

    2

---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,14 @@
+
+import style from "./AboutMe.module.scss"
+
+
+
+function AboutMe() {
+
+    return (
+        <section className={style.AboutMe}>
+        </section>
+    )
+}
+
+export default AboutMe
