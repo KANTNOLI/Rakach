85fac37bc8cb29840d80a28b0db90d1383086da7
KANTNOLI <zahargysev02@gmail.com>
2025-08-03 12:42:02 +0300

    1

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,4 +1,4 @@
-import { motion } from "motion/react"
+// import { motion } from "motion/react"
 
 import stylePreview from "./Preview.module.scss";
 import styleExample from "./Example.module.scss";
